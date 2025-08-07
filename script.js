598562293bcb8a5ac1a08d558c2ab70737d8d874
Solongo Nyam-Ochir <solongos091@gmail.com>
2025-08-07 16:58:12 +0800

    solongo portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+// let tl = gsap.timeline({
+// 	// yes, we can add it to an entire timeline!
+// 	scrollTrigger: {
+		
+// 		snap: {
+// 			snapTo: 'sections', // snap to the closest label in the timeline
+// 			duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+// 			delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+// 			ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+// 		}
+// 	}
+// });
+
+
+// window.addEventListener('scroll', function () {
+// const header = document.getElementById('header');
+// if (window.scrollY > 50) {
+//     header.classList.add('scrolled');
+// } else {
+//     header.classList.remove('scrolled');
+// }
+// });
+
+
+// const toggle = document.getElementById('menu-toggle');
+//   const nav = document.getElementById('nav');
+
+//   toggle.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+//   });
