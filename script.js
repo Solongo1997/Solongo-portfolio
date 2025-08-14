6ec067c2e538b86936731d8d821b005ea1ceaac3
Solongo Nyam-Ochir <solongos091@gmail.com>
2025-08-14 15:39:29 +0800

    install express cors

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-// let tl = gsap.timeline({
-// 	// yes, we can add it to an entire timeline!
-// 	scrollTrigger: {
-		
-// 		snap: {
-// 			snapTo: 'sections', // snap to the closest label in the timeline
-// 			duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-// 			delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-// 			ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
-// 		}
-// 	}
-// });
-
-
-// window.addEventListener('scroll', function () {
-// const header = document.getElementById('header');
-// if (window.scrollY > 50) {
-//     header.classList.add('scrolled');
-// } else {
-//     header.classList.remove('scrolled');
-// }
-// });
-
-
-// const toggle = document.getElementById('menu-toggle');
-//   const nav = document.getElementById('nav');
-
-//   toggle.addEventListener('click', () => {
-//     nav.classList.toggle('active');
-//   });
+fetch('http://localhost:5000/api/projects')
+  .then(res => res.json())
+  .then(projects => {
+    const container = document.getElementById('project-section');
+    container.innerHTML = projects.slice(0, 3).map(p => `
+      <div class="card">          
+        <figure class="card-img">
+          <a href="${p.link}" target="_blank">
+            <img src="${p.image}" alt="${p.title}" />
+            <figcaption><span>${p.tech}</span></figcaption>
+          </a>
+        </figure>
+        <div class="card-content">
+          <h4>${p.title}</h4>
+          <a href="${p.link}" target="_blank">${p.link}</a>
+        </div>
+      </div>
+    `).join('');
+  });
